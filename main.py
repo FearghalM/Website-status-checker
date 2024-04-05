2508f132f2223d7a09489d4f36b9077885164bae
FearghalM <femcmorrow@gmail.com>
2024-04-05 11:44:32 +0100

    Add code from main to seperate functions for readability

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 # Lock for thread-safe writing
 write_lock = Lock()
 
+
 # Function to check redirect for a single URL
 def check_redirect(url):
     try:
@@ -33,6 +34,29 @@ def cleanData(data):
     data = [row for row in data if any(row)]
     return data
 
+def process_urls_with_threadpool(urls):
+    results = []
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        for result in executor.map(check_redirect, urls):
+            results.append(result)
+            logger.info(f"Processed URL: {result[0]}")
+    return results
+
+def write_data_to_csv(file_path, header, updated_data):
+    """
+    Write the modified data back to the CSV file after processing all URLs.
+    
+    Parameters:
+    - file_path (str): The path to the CSV file.
+    - header (list): The header of the CSV file.
+    - updated_data (list of lists): The updated data to be written to the CSV file.
+    """
+    with write_lock:
+        with open(file_path, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(header)  # Write header
+            csv_writer.writerows(updated_data)  # Write updated data
+
 # Main function
 def main(file_path):
     try:
@@ -41,24 +65,17 @@ def main(file_path):
             # Clean the data removing empty rows
             data = cleanData(list(csv_reader))
 
-        header = data[0]#Default header = Domain,Redirect URL, Status Code
+        header = data[0]# Default header = Domain,Redirect URL, Status Code
         urls = [row[0] for row in data[1:]]
 
-        results = []
-        with ThreadPoolExecutor(max_workers=20) as executor:
-            for result in executor.map(check_redirect, urls):
-                results.append(result)
-                logger.info(f"Processed URL: {result[0]}")
+        # Call the function to process URLs using ThreadPoolExecutor
+        results = process_urls_with_threadpool(urls)
 
         # Combine the results with the original data
         updated_data = [[url, redirect_url, status_code] for url, redirect_url, status_code in results]
 
-        # Write the modified data back to the CSV file after processing all URLs
-        with write_lock:
-            with open(file_path, 'w', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(header)  # Write header
-                csv_writer.writerows(updated_data)  # Write updated data
+        # Call the function to write data to CSV
+        write_data_to_csv(file_path, header, updated_data)
 
         logger.info("CSV file successfully modified.")
     except FileNotFoundError:
